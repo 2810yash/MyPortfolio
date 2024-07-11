d3a49ba41943ed5794c7dc97df3483fda5c2e9a2
2810yash <yash2810203@gmail.com>
2024-07-12 03:54:15 +0530

    new resume updated again

--- lib/src/constants/my_handlers.dart
+++ lib/src/constants/my_handlers.dart
@@ -1,6 +1,6 @@
 
 class MyUrls {
-  static const String myResume = "https://drive.google.com/file/d/10ykuyK2buXBYG8wBmHiaU2Chj67JnVGt/view?usp=sharing";
+  static const String myResume = "https://drive.google.com/file/d/10ykuyK2buXBYG8wBmHiaU2Chj67JnVGt/view";
   static const String instagram = "https://www.instagram.com/_ash_2810/";
   static const String gitHub = "https://github.com/2810yash";
   static const String twitter = "https://twitter.com/_Yash_Wairagade?t=BLuBi93t0cLm_QAbZuHgPA&s=08";
